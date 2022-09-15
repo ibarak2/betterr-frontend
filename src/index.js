07c4270d0066eea8d2fe6668135033662d51973b
Zrizony <royyam99@gmail.com>
2022-09-15 19:23:28 +0300

    changed sass module, home-page - v0.1 first skeleton

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import "./assets/styles/main.scss"
 import { BrowserRouter as Router } from "react-router-dom"
 import { Provider } from "react-redux"
 
 import { store } from "./store/store"
 import { RootCmp } from "./root-cmp"
-import "./assets/styles/main.scss"
 
 ReactDOM.render(
   <React.StrictMode>
