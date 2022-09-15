9b430425896ac592bd22e905dab32503d0741c01
elliot sherman <sheveryosef@gmail.com>
2022-09-15 18:05:25 +0300

    commiting after merge

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -4,11 +4,8 @@ import routes from "./routes"
 
 import { AppHeader } from "./cmps/app-header"
 import { AppFooter } from "./cmps/app-footer"
-<<<<<<< HEAD
 import { UserDetails } from "./pages/user-details"
 import { GigPreview } from "./cmps/gig-preview"
-=======
->>>>>>> c7c2ce3b22a0218eca8ed6ede68f3da20ecf75f7
 
 // export class RootCmp extends React.Component {
 export function RootCmp() {
