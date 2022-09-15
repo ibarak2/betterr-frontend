b85d333a847ed8c2c44551ee0ea66c6afd51d0ec
elliot sherman <sheveryosef@gmail.com>
2022-09-15 19:40:23 +0300

    merging after sass fix

--- src/cmps/gig-preview.jsx
+++ src/cmps/gig-preview.jsx
@@ -4,20 +4,53 @@ import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos"
 // import { GigCarousel } from "./gig-carousel"
 
 export const GigPreview = () => {
-  const imgs = ["1", "2", "3"]
+  // let slideIndex = 1;
+  // showSlides(slideIndex);
+
+  // function plusSlides(n) {
+  //   showSlides(slideIndex += n);
+  // }
+
+  // function currentSlide(n) {
+  //   showSlides(slideIndex = n);
+  // }
+
+  // function showSlides(n) {
+  //   let i;
+  //   let slides = document.getElementsByClassName("mySlides");
+  //   let dots = document.getElementsByClassName("demo");
+  //   let captionText = document.getElementById("caption");
+  //   if (n > slides.length) {slideIndex = 1}
+  //   if (n < 1) {slideIndex = slides.length}
+  //   for (i = 0; i < slides.length; i++) {
+  //     slides[i].style.display = "none";
+  //   }
+  //   for (i = 0; i < dots.length; i++) {
+  //     dots[i].className = dots[i].className.replace(" active", "");
+  //   }
+  //   slides[slideIndex-1].style.display = "block";
+  //   dots[slideIndex-1].className += " active";
+  //   captionText.innerHTML = dots[slideIndex-1].alt;
+  // }
+  const imgs = [
+    "1",
+    // , "2", "3"
+  ]
   return (
     <div className="gig-preview">
-      <div className="img-preview">
-        {imgs.map((img) => (
-          <img key={img} src={`https://picsum.photos/200/300?random=${img}`} />
-        ))}
-      </div>
-      <button className="btn-preview">
+      <div className="slideshow-container">
+        <img
+          className="img-preview"
+          key={1}
+          src={`https://picsum.photos/200/300?random=${1}`}
+        />
+      <button className="btn-preview prev">
         <ArrowBackIosIcon />
       </button>
-      <button className="btn-preview">
+      <button className="btn-preview next">
         <ArrowForwardIosIcon />
       </button>
+      </div>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react"
+import "./assets/styles/main.scss"
 import ReactDOM from "react-dom"
 import { BrowserRouter as Router } from "react-router-dom"
 import { Provider } from "react-redux"
 
 import { store } from "./store/store"
 import { RootCmp } from "./root-cmp"
-import "./assets/styles/main.scss"
 
 ReactDOM.render(
   <React.StrictMode>
