f6eff762fa7cae3d441f82cade980bc201f5b717
elliot sherman <sheveryosef@gmail.com>
2022-09-16 17:12:01 +0300

    add data to be rendered by each preview from the redux store

--- src/cmps/gig-list.jsx
+++ src/cmps/gig-list.jsx
@@ -1,14 +1,30 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
 import { Filter } from "./filter"
 import { GigPreview } from "./gig-preview"
+import { loadGigs } from "../store/gig.actions"
+
 export const GigList = () => {
-  const gigs = ["1", "2", "3", "4", "5", "6"]
+  const gigs = useSelector((state) => state.gigModule.gigs)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(loadGigs())
+    console.log("GIGS", gigs)
+  }, [])
+  const gigsArr = [
+    "1",
+    "2",
+    // "3", "4", "5", "6"
+  ]
+  if (!gigs) return <h1>Loading...</h1>
   return (
-    <div className="gig-list" style={{display:'flex',}}>
+    <div className="gig-list" style={{ display: "flex" }}>
       <Filter />
       <div className="gigs">
-        {gigs.map((gig) => (
-          <GigPreview />
-        ))}
+        {gigs.map((gig) => {
+          console.log(gig)
+          return <GigPreview key={gig._id} {...gig}/>
+        })}
       </div>
     </div>
   )
