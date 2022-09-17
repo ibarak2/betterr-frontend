bcf9cf884aae6872683285b8496143fe84548d27
Zrizony <royyam99@gmail.com>
2022-09-18 00:53:56 +0300

    app-header v0.6 - added second navbar for categories

--- src/cmps/secondary-navbar.jsx
+++ src/cmps/secondary-navbar.jsx
@@ -1,16 +1,46 @@
-import { NavLink } from "react-router-dom"
-
+import { NavLink, Route, Routes } from 'react-router-dom'
 
 export const SecondaryNavbar = () => {
 
+  const categories = [
+    'Digital Marketing',
+    'Writing & Translation',
+    'Video & Animation',
+    'Music & Audio',
+    'Programming & Tech',
+    'Business',
+    'Lifestyle',
+    'Trending',
+  ]
 
   return (
     <nav className="max-width-container secondary-navbar">
-      <div className="flex secondary-nav-routes">
-        <NavLink to="/?nav=home">Home</NavLink>
-        <NavLink to="/explore">Explore</NavLink>
-        <NavLink to="/about">About Us</NavLink>
-      </div>
+      <ul className="flex clean-list secondary-nav-routes">
+        <li>
+          <NavLink to="/?nav=graphics-design">Graphics & Design</NavLink>
+        </li>
+        <li>
+          <NavLink to="/?nav=Writing & Translation">Writing & Translation</NavLink>
+        </li>
+        <li>
+          <NavLink to="/?nav=Video & Animation">Video & Animation</NavLink>
+        </li>
+        <li>
+          <NavLink to="/?nav=Music & Audio">Music & Audio</NavLink>
+        </li>
+        <li>
+          <NavLink to="/?nav=Programming & Tech">Programming & Tech</NavLink>
+        </li>
+        <li>
+          <NavLink to="/?nav=Business">Business</NavLink>
+        </li>
+        <li>
+          <NavLink to="/?nav=Lifestyle">Lifestyle</NavLink>
+        </li>
+        <li>
+          <NavLink to="/?nav=Trending">Trending</NavLink>
+        </li>
+      </ul>
     </nav>
   )
 }
