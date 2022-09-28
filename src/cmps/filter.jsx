07c33444afae6ab19b4299491e1d78d38d3a7b1c
elliot sherman <sheveryosef@gmail.com>
2022-09-28 23:12:51 +0300

    redesign filters on explore page like on fiverr website

--- src/cmps/filter.jsx
+++ src/cmps/filter.jsx
@@ -1,27 +1,111 @@
+import { Fragment, useState } from "react"
 import { useFormRegister } from "../hooks/useFormRegister"
 
 export const Filter = ({ onChangeFilter }) => {
-  const [register] = useFormRegister(
+  const [maxPriceOpen, setMaxPriceOpen] = useState(false)
+  const [maxDeliveryOpen, setMaxDeliveryOpen] = useState(false)
+  const [maxRatingOpen, setMaxRatingOpen] = useState(false)
+  const [register, setFields] = useFormRegister(
     {
       basicPrice: "",
       basicDaysToMake: "",
-      rate: ""
+      rate: "",
     },
     onChangeFilter
   )
-  return (
-    <form className="filter">
-      <section>
-        <input {...register("basicPrice", "number")} placeholder="Max Price" />
-      </section>
-      <section>
-        <input {...register("basicDaysToMake", "number")} placeholder="Delivery Time" />
-      </section>
-      <section>
-        <input {...register("rate", "number")} placeholder="Rating" />
-      </section>
+  const filterSettings = [
+    {
+      text: "Max Price",
+      handler: () => setMaxPriceOpen(!maxPriceOpen),
+      state: maxPriceOpen,
+      placeholder: "Max Price",
+      name: "basicPrice",
+      type: "number",
+    },
+    {
+      text: "Delivery Time",
+      handler: () => setMaxDeliveryOpen(!maxDeliveryOpen),
+      state: maxDeliveryOpen,
+      placeholder: "Delivery Time",
+      name: "basicDaysToMake",
+      type: "number",
+    },
+    {
+      text: "Rating",
+      handler: () => setMaxRatingOpen(!maxRatingOpen),
+      state: maxRatingOpen,
+      placeholder: "Rating",
+      name: "rate",
+      type: "number",
+    },
+  ]
+  const filters = filterSettings.map((filter) => (
+    <div
+      className="flex align-center fiverr-style-filter"
+      onClick={filter.handler}
+    >
+      <div className="text">{filter.text}</div>
+      <span className={`filter-chevron ${filter.state && "open"}`}>
+        <ChevronSvgIcon />
+      </span>
+      <form
+      className="form-modal"
+        style={{
+          display: `${filter.state ? "" : "none"}`,
+        }}
+      >
+        <section>
+          <input
+            {...register(`${filter.name}`, `${filter.type}`)}
+            placeholder={`${filter.placeholder}`}
+          />
+        </section>
+      </form>
+    </div>
+  ))
 
-      <button className="btn btn-reset-filter" onClick={() => register("maxPrice", "")}>reset</button>
-    </form>
+  return (
+    <Fragment>
+      <section className="flex filter-options">
+        {filters}
+        <button
+          className="btn btn-reset-filter"
+          onClick={() =>
+            setFields({ basicPrice: "", basicDaysToMake: "", rate: "" })
+          }
+        >
+          reset
+        </button>
+      </section>
+      {/* <form className="filter">
+        <section>
+          <input
+            {...register("basicPrice", "number")}
+            placeholder="Max Price"
+          />
+        </section>
+        <section>
+          <input
+            {...register("basicDaysToMake", "number")}
+            placeholder="Delivery Time"
+          />
+        </section>
+        <section>
+          <input {...register("rate", "number")} placeholder="Rating" />
+        </section>
+      </form> */}
+    </Fragment>
+  )
+}
+const ChevronSvgIcon = () => {
+  return (
+    <svg
+      width="11"
+      height="7"
+      viewBox="0 0 11 7"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M5.4636 6.38899L0.839326 1.769C0.692474 1.62109 0.692474 1.38191 0.839326 1.23399L1.45798 0.61086C1.60483 0.462945 1.84229 0.462945 1.98915 0.61086L5.72919 4.34021L9.46923 0.61086C9.61608 0.462945 9.85354 0.462945 10.0004 0.61086L10.619 1.23399C10.7659 1.38191 10.7659 1.62109 10.619 1.769L5.99477 6.38899C5.84792 6.5369 5.61046 6.5369 5.4636 6.38899Z"></path>
+    </svg>
   )
 }
